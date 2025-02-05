e8044c22735e2850b52e545df7c8f31c9e1248f2
EES <EES@oit-v.elordaeco.kz>
2025-02-05 15:11:42 +0500

    Initial commit

---
+++ src/components/Movie.vue
@@ -0,0 +1,82 @@
+<script setup>
+const props = defineProps({
+    movie: {
+        type:Object,
+        required:true,
+        default: () => {}
+    }
+})
+console.log(movie.poster_path)
+</script>
+
+<template>
+    <div class="movie">
+        <img :src="`./asstes/img/${movie.poster_path}`" :alt="movie.poster_path" class="movie-img">
+        <div>
+            <div class="movie-name">
+                {{ movie.original_title }}
+                {{ movie.release_date }}
+                <div class="movie-overview">{{ movie.overview }}</div>
+                {{ movie.isWatched ? "Yes" : "No"}}
+            </div>
+        </div>
+        
+    </div>
+
+</template>
+
+<style scoped>
+.movie {
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  column-gap: 30px;
+  margin-bottom: 20px;
+  border: 2px solid #efefef;
+  padding: 10px;
+  border-radius: 10px;
+}
+
+.movie-accept {
+  margin-right: 10px;
+}
+
+.movie-img {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+
+.movie-name {
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  margin-bottom: 20px;
+}
+
+.movie-overview {
+  display: block;
+  margin-bottom: 20px;
+}
+
+.movie-buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.movie-buttons-watched {
+  color: #fff;
+  background: #1eb4c3;
+}
+
+.movie-buttons-watched__icon {
+  width: 15px;
+  margin-left: 10px;
+}
+
+.movie-buttons-delete {
+  color: #fff;
+  background: #ff2a2a;
+}
+</style>
---
+++ src/stores/MovieStore.js
@@ -0,0 +1,43 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+// export const useCounterStore = defineStore('counter', () => {
+//   const count = ref(0)
+//   const doubleCount = computed(() => count.value * 2)
+//   function increment() {
+//     count.value++
+//   }
+
+//   return { count, doubleCount, increment }
+// })
+
+export const useMovieStore = defineStore("movieStore", {
+  state: () => ({
+    movies: [
+      {
+        id: 1,
+        original_title: "Inception",
+        overview: "A thief who enters the dreams of others to steal secrets from their subconscious is given a chance to erase his past crimes.",
+        poster_path: "Inception.webp",
+        release_date: "2010-07-16",
+        isWatched: true
+      },
+      {
+        id: 2,
+        original_title: "The Matrix",
+        overview: "A computer hacker learns about the true nature of reality and his role in the war against its controllers.",
+        poster_path: "The_Matrix.webp",
+        release_date: "1999-03-31",
+        isWatched: false
+      },
+      {
+        id: 3,
+        original_title: "Interstellar",
+        overview: "A team of explorers travels through a wormhole in space in an attempt to ensure humanity's survival.",
+        poster_path: "Interstellar.webp",
+        release_date: "2014-11-07",
+        isWatched: true
+      }
+    ]
+  })
+})
